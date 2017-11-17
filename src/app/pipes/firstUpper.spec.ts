f7caec02577e11474b720d54f18bb43d53d70ded
DimaBooka <atlasb777@gmail.com>
2017-11-17 12:16:09 +0200

    added test on pipe

---
+++ src/app/pipes/firstUpper.spec.ts
@@ -0,0 +1,15 @@
+import { TitleCasePipe } from "./firstUpper";
+
+describe('TitleCasePipe', () => {
+  // This pipe is a pure, stateless function so no need for BeforeEach
+  let pipe = new TitleCasePipe();
+
+  it('transforms "abc" to "Abc"', () => {
+    expect(pipe.transform('abc')).toBe('Abc');
+  });
+
+  it('transforms "abc def" to "Abc Def"', () => {
+    expect(pipe.transform('abc def')).toBe('Abc Def');
+  });
+
+});
